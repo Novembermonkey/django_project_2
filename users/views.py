cadf2acf78cc41df65fd1f4aadf0c5dc6e81af22
Novembermonkey <djodio2023@gmail.com>
2025-05-27 14:46:45 +0500

    final

--- users/views.py
+++ users/views.py
@@ -14,28 +14,28 @@ def login_page(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(email=cd['email'], password=cd['password'])
+            user = authenticate(request, email=cd['email'], password=cd['password'])
             if user:
                 login(request, user)
-                return redirect('shop:product_list')
+                return redirect('shop:index')
             else:
                 messages.add_message(
                     request,
                     messages.ERROR,
                     "Username or Password is incorrect"
                 )
-                pass
+
     return render(request, 'users/login.html', {'form': form})
 
 def logout_page(request):
     if request.method == 'POST':
         logout(request)
-        return redirect('shop:product_list')
+        return redirect('shop:index')
 
 def register_page(request):
-    form = RegisterForm()
+    form = RegisterModelForm()
     if request.method == 'POST':
-        form = RegisterForm(request.POST)
+        form = RegisterModelForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])
